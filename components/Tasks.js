f30bcdd36d457dd6c15b97cd467946b53709209d
Saeed <saeed.karimi@generalinternet.ca>
2022-12-14 15:55:07 -0800

    updated scrollview to flatlist for better ui

--- components/Tasks.js
+++ components/Tasks.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, ScrollView, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Text, SafeAreaView, ScrollView, StyleSheet, ActivityIndicator, FlatList } from 'react-native';
 import React, { useLayoutEffect, useState, useEffect } from 'react';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -19,18 +19,48 @@ const Tasks = ({ navigation }) => {
 						Authorization: `${token}`,
 					},
 				});
-				console.log(response.data.tasks);
 				setTasks(response.data.tasks);
+				console.log(tasks);
 				setLoading(false);
 			}
 		} catch (error) {
 			console.log(error);
 		}
 	};
-
+	
 	useEffect(() => {
 		getTasks();
 	}, []);
+
+
+	const Task = ({ name, duedate, status, status_name }) => {
+		return (
+			<View className="border border-gray-200 rounded-md p-2 mb-2 bg-white">
+				<Text className="text-md pb-2 border-bottom-1 border-gray-200">{name}</Text>
+				<View className="flex flex-row justify-between">
+					{duedate !== null ? (
+						<Text className="text-xs text-gray-500">Due Date: {duedate}</Text>
+					) : (
+						<Text className="text-xs text-gray-500">No Due Date</Text>
+					)}
+					{/* tailwindcss class based on status */}
+					{status == 1 ? (
+						<Text className="text-sm">{status_name}</Text>
+					) : status == 4 ? (
+						<Text className="text-sm text-blue-500">{status_name}</Text>
+					) : status == 5 ? (
+						<Text className="text-sm text-green-500">{status_name}</Text>
+					) : status == 2 ? (
+						<Text className="text-sm text-red-500">{status_name}</Text>
+					) : (
+						<Text className="text-sm text-gray-500">{status_name}</Text>
+					)}
+				</View>
+			</View>
+		);
+	};
+
+	const renderItem = ({ item }) => <Task name={item.name} duedate={item.duedate} status={item.status} status_name={item.status_name} />;
 
 	return loading ? (
 		<View className="flex-1 items-center justify-center align-center mt-5">
@@ -38,32 +68,14 @@ const Tasks = ({ navigation }) => {
 		</View>
 		) : (
 		<SafeAreaView className="rounded-md">
-				<ScrollView className="flex flex-col mt-4" showsVerticalScrollIndicator={false}>
-						{tasks.map((task) => (
-						<View key={task.id} className="border border-gray-200 rounded-md p-2 mb-2 bg-white">
-							<Text className="text-md pb-2 border-bottom-1 border-gray-200">{task.name}</Text>
-							<View className="flex flex-row justify-between">
-								{task.duedate !== null ? (
-									<Text className="text-xs text-gray-500">Due Date: {task.duedate}</Text>
-								) : (
-									<Text className="text-xs text-gray-500">No Due Date</Text>
-								)}
-								{/* tailwindcss class based on status */}
-								{task.status == 1 ? (
-									<Text className="text-sm">{task.status_name}</Text>
-								) : task.status == 4 ? (
-									<Text className="text-sm text-blue-500">{task.status_name}</Text>
-								) : task.status == 5 ? (
-									<Text className="text-sm text-green-500">{task.status_name}</Text>
-								) : task.status == 2 ? (
-									<Text className="text-sm text-red-500">{task.status_name}</Text>
-								) : (
-									<Text className="text-sm text-gray-500">{task.status_name}</Text>
-								)}
-							</View>
-						</View>
-					))}
-				</ScrollView>
+			<FlatList
+				data={tasks}
+				renderItem={renderItem}
+				keyExtractor={(task) => task.id}
+				listEmptyComponent={<Text className="text-center text-gray-500">No Tasks</Text>}
+				showsVerticalScrollIndicator={false}
+				listFooterComponent={<View className="h-10" />}
+			/>
 		</SafeAreaView>
 	);
 };
